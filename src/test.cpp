1c1066ade066f83a76c0989183b198cd8d090dab
mqnc <mirko.kunze@web.de>
2024-02-12 22:33:27 +0100

    simple testing suite

--- src/test.cpp
+++ src/test.cpp
@@ -1,15 +1,67 @@
 
+#include <string>
+#include <iostream>
 #include "parse.hpp"
 #include "stringify.hpp"
 
-int main(){
+#define THROW_ON_FAILURE
 
-	JSON5::Value j1 = JSON5::parse("{JSON:5,}");
-	JSON5::Value j2 = JSON5::Object{{"JSON", 5.0}};
+size_t numPassed = 0;
+size_t numFailed = 0;
 
-	std::cout << JSON5::stringify(j1) << "\n";
+void expect(bool success, std::string msg) {
+	if (success) {
+		std::cout << "\033[32mpassed: " << msg << "\033[0m\n";
+		numPassed++;
+	}
+	else {
+		std::string fail = std::string("\033[31mfailed: ") + msg + "\033[0m\n";
+#ifdef THROW_ON_FAILURE
+		throw std::runtime_error(fail);
+#else
+		std::cerr << fail;
+		numFailed++;
+#endif
+	}
+}
 
-	std::cout << (j1 == j2) << "\n";
-	
+int main() {
+
+	expect(
+		JSON5::parse("{}")
+			== JSON5::Object {},
+		"parses empty objects"
+	);
+
+	expect(
+		JSON5::parse("{\"a\":1}")
+			== JSON5::Object {{"a", 1.0}},
+		"parses double string property names"
+	);
+
+	expect(
+		JSON5::parse("{'a':1}")
+			== JSON5::Object {{"a", 1.0}},
+		"parses single string property names"
+	);
+
+	expect(
+		JSON5::parse("{a:1}")
+			== JSON5::Object {{"a", 1.0}},
+		"parses unquoted property names"
+	);
+
+	expect(
+		JSON5::parse("{$_:1,_$:2,a\u200C:3}")
+			== JSON5::Object {{"$_", 1.0}, {"_$", 2.0}, {"a\u200C", 3.0}},
+		"parses special character property names"
+	);
+
+	expect(
+		JSON5::parse("{ùńîċõďë:9}")
+			== JSON5::Object {{"ùńîċõďë", 9.0}},
+		"parses unicode property names"
+	);
+
 	return EXIT_SUCCESS;
 }
