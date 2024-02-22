2f26f662eddc1e694d3b52c1ec9008bdd106a3e9
mqnc <mirko.kunze@web.de>
2024-02-22 20:13:00 +0100

    display build/test/coverage badges on readme

--- build_test_coverage.sh
+++ build_test_coverage.sh
@@ -7,18 +7,23 @@ mkdir build
 cd build
 mkdir report
 
-BUILD_STATUS_BADGE=report/build_status.svg
-TEST_STATUS_BADGE=report/test_status.svg
-COVERAGE_BADGE=report/coverage.svg
+BUILD_LOG=report/build_log.txt
+TEST_LOG=report/test_log.txt
 DETAILED_COVERAGE=report/detailed_coverage.html
 COVERAGE_SUMMARY=report/coverage_summary.txt
 
+BUILD_STATUS_BADGE=report/build_status.svg
+TEST_STATUS_BADGE=report/test_status.svg
+COVERAGE_BADGE=report/coverage.svg
+
 echo building test_suite...
 {
-	clang++ -std=c++17 -I../src -g -Wall -Wextra -Wpedantic -Werror -fprofile-instr-generate -fcoverage-mapping -o test_suite ../test/main.cpp
+	clang++ -std=c++17 -I../src -g -Wall -Wextra -Wpedantic -Werror -fprofile-instr-generate -fcoverage-mapping -o test_suite ../test/main.cpp > $BUILD_LOG
+	cat $BUILD_LOG
 	echo build successful
 	curl https://img.shields.io/badge/build-passing-brightgreen > $BUILD_STATUS_BADGE
 } || {
+	cat $BUILD_LOG
 	echo build failed
 	curl https://img.shields.io/badge/build-failing-red > $BUILD_STATUS_BADGE
 	curl https://img.shields.io/badge/tests-not_run-silver > $TEST_STATUS_BADGE
@@ -28,10 +33,12 @@ echo building test_suite...
 
 echo running tests...
 {
-	LLVM_PROFILE_FILE="test.profraw" ./test_suite
+	LLVM_PROFILE_FILE="test.profraw" ./test_suite > $TEST_LOG
+	cat $TEST_LOG
 	echo tests passed
 	curl https://img.shields.io/badge/tests-passing-brightgreen > $TEST_STATUS_BADGE
 } || {
+	cat $TEST_LOG
 	echo tests failed
 	curl https://img.shields.io/badge/tests-failing-red > $TEST_STATUS_BADGE
 }
